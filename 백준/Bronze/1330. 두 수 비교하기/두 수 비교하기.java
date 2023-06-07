669d80f6c9e67725fc8bb1011093cc417fba0344
한유경 <74157075+funhappyit@users.noreply.github.com>
2023-06-08 05:30:12 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 212 ms, Memory: 17576 KB -BaekjoonHub

--- 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -1,18 +1,32 @@
 import java.util.Scanner;
 public class Main {
- 
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		
-		int A = in.nextInt();
-		int B = in.nextInt();
-		
-		in.close();
+    public static void main(String[] args) {
+        /*
+        문제:두 정수 A와 B가 주어졌을 때, A와 B를 비교하는 프로그램을 작성하시오.
+        입력:첫째 줄에 A와 B가 주어진다. A와 B는 공백 한 칸으로 구분되어져 있다.
+        출력:첫째 줄에 다음 세 가지 중 하나를 출력한다.
+            A가 B보다 큰 경우에는 '>'를 출력한다.
+            A가 B보다 작은 경우에는 '<'를 출력한다.
+            A와 B가 같은 경우에는 '=='를 출력한다.
+        제한:-10,000 ≤ A, B ≤ 10,000
+        ex) 1 2
+        */
+        Scanner sc = new Scanner(System.in);
         
-		if(A>B) System.out.println(">");
-		else if(A<B) System.out.println("<");
-		else System.out.println("==");
- 
-	}
- 
+        String str = sc.nextLine();
+        String[] strList = str.split(" ");
+        if(strList.length >= 2 && strList != null){
+            int A = Integer.parseInt(strList[0]);
+            int B = Integer.parseInt(strList[1]);
+            if(A >= -10000 && B <= 10000){
+                if(A>B){
+                    System.out.println(">");
+                }else if(A<B){
+                    System.out.println("<");
+                }else{
+                    System.out.println("==");
+                }
+            }
+        }
+    }
 }
