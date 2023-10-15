54a7e24d24e6d8f4761b7c15f8f30ec1a6d3eb22
한유경 <74157075+funhappyit@users.noreply.github.com>
2023-10-15 13:20:16 +0900

    Update 평균.java

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,22 +1,27 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // 선언
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int cnt = sc.nextInt();
+        sc.nextLine();
+        String str = sc.nextLine();
+        String[] strArray = str.split(" ");
+        double result = 0;
+        int maxCnt = Integer.valueOf(strArray[0]);
+        for(int i=0;i<cnt;i++){
+            int tempCnt = Integer.valueOf(strArray[i]);
+            if(tempCnt>maxCnt){
+                maxCnt = tempCnt;
+            }
+        }
+        for(int j=0;j<cnt;j++){
+            Double tmpCnt = Double.valueOf(strArray[j]);
 
-		int A = Integer.parseInt(br.readLine());
-		String score = br.readLine();
-		int[] scoreList = Arrays.stream(score.split(" ")).mapToInt(Integer::parseInt).sorted().toArray();
-
-		int max = scoreList[A - 1];
-		double sum = 0;
-
-		for (int i = 0; i < scoreList.length; i++) {
-			sum += scoreList[i] / Double.valueOf(max * 100) * 10000;
-		}
-		System.out.println(Double.valueOf(sum / A));
-	}
+            Double tmpResult = tmpCnt / maxCnt*100;
+            result += tmpResult;
+        }
+        result = result/cnt;
+        System.out.print(result);
+    }
 }
