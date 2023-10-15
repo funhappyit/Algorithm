5a720c0c191506f6a388327a53bb912099e41d0c
한유경 <74157075+funhappyit@users.noreply.github.com>
2023-10-15 15:30:45 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1856 ms, Memory: 74600 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,22 +1,34 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+
+
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // 선언
-		int[] firstInput = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-		int[] secondInput = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-		int N = firstInput[0];
-		int M = firstInput[1];
-		int [] resultArr = new int[N+1];
-		resultArr[0] = 0;
-		for(int i=1;i<resultArr.length;i++){
-			resultArr[i] = resultArr[i-1]+secondInput[i-1];
-		}
-		for(int j=0;j<M;j++){
-			int[] index =  Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-			System.out.println(resultArr[index[1]]-resultArr[index[0]-1]);
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
+        String firStr = sc.readLine();
+        String secStr = sc.readLine();
+        String[] firStrArr = firStr.split(" ");
+        String[] secStrArr = secStr.split(" ");
+
+        int dataCnt = Integer.valueOf(firStrArr[0]);
+        int questCnt = Integer.valueOf(firStrArr[1]);
+
+        Long[] sumArray = new Long[dataCnt+1];
+        sumArray[0] = 0L;
+        sumArray[1] = Long.valueOf(secStrArr[0]);
+        for(int j=1;j<=dataCnt;j++){
+           if (j != 1){
+               sumArray[j] = sumArray[j-1]+ Long.valueOf(secStrArr[j-1]);
+            }
+        }
+
+        for(int i=0;i<questCnt;i++){
+            String thStr = sc.readLine();
+            String[] thiStrArr = thStr.split(" ");
+            int firIndex = Integer.valueOf(thiStrArr[0])-1;
+            int lastIndex = Integer.valueOf(thiStrArr[1]);
+            System.out.println(sumArray[lastIndex]-sumArray[firIndex]);
+        }
+    }
 }
