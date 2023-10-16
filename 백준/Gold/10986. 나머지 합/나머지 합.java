0b1c77aa38752d17f610a3959c269f966f50f954
한유경 <74157075+funhappyit@users.noreply.github.com>
2023-10-16 11:42:12 +0900

    [Gold III] Title: 나머지 합, Time: 524 ms, Memory: 143920 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.java
+++ 백준/Gold/10986. 나머지 합/나머지 합.java
@@ -1,48 +1,47 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Map;
-import java.util.StringTokenizer;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // 선언
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		Long result = 0L;
-		st = new StringTokenizer(br.readLine());
-		Long[] arr = new Long[N];
-		Long[] restArr = new Long[N];
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		String firStr = bf.readLine();
+		String secStr = bf.readLine();
+		String[] firStrArr = firStr.split(" ");
+		String[] secStrArr = secStr.split(" ");
 
-		arr[0] = Long.valueOf(st.nextToken());
-		for(int j=1;j<N;j++){
-			Long num =  Long.valueOf(st.nextToken());
-			arr[j] = num+arr[j-1];
-		}
+		int cnt = Integer.valueOf(firStrArr[0]);
+		int diviCnt = Integer.valueOf(firStrArr[1]);
+		long result = 0;
 
-		for(int i=0;i<arr.length;i++){
-			int rest = (int)(arr[i]%M);
-			restArr[i] = arr[i]%M;
-			if(rest == 0){
-				result++;
+		long[] arrA = new long[cnt];
+		long[] arrC = new long[diviCnt];
+
+		for(int i=0;i<cnt;i++){
+			int rest = 0;
+			if(i == 0){
+				rest = (Integer.valueOf(secStrArr[0]))%diviCnt;
+				arrA[0] = rest;
+				arrC[rest]++;
+				if(rest == 0){
+					result += 1;
+				}
+			}else{
+				rest = (Integer.valueOf((int)arrA[i-1])+Integer.valueOf(secStrArr[i]))%diviCnt;
+				arrA[i] = rest;
+				arrC[rest]++;
+				if(rest == 0){
+					result += 1;
+				}
 			}
 
 		}
 
-		Stream<Long> longStream = Arrays.stream(restArr);
-		Map<Long, Long> map = longStream.collect(
-			Collectors.toMap(Function.identity(), value -> 1L, Long::sum)
-		);
-
-		for (Map.Entry<Long, Long> pair : map.entrySet()) {
-			if(pair.getValue() > 1){
-				Long cnt = (pair.getValue()*(pair.getValue()-1))/2;
-				result += cnt;
+		for(int i=0;i<diviCnt;i++){
+			long cNum = arrC[i];
+			if(cNum !=0){
+				long sum = (cNum*(cNum-1))/2;
+				result+=sum;
 			}
 		}
 		System.out.println(result);
